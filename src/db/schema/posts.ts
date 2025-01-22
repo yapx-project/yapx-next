69566427bc35b8831914e8f2e9c3fb6b8a25075b
Dmitriy Pervin <theforner@gmail.com>
2025-01-22 17:28:32 +0500

    Update posts.ts

--- src/db/schema/posts.ts
+++ src/db/schema/posts.ts
@@ -1,4 +1,4 @@
-import {sqliteTable, integer, text} from "drizzle-orm/sqlite-core"
+import {sqliteTable, integer, text, foreignKey, index} from "drizzle-orm/sqlite-core"
 import {created_at, updated_at} from "@/db/schema/helpers/timestamp.helpers";
 import {relations} from "drizzle-orm";
 import {users} from "@/db/schema/users";
@@ -10,16 +10,33 @@ export const posts = sqliteTable('posts', {
     text: text(),
 
     owner_id: integer('owner_id')
+        .notNull()
         .references(
             () => users.id,
             {onDelete: 'cascade'}
         ),
+    reply_post_id: integer('reply_post_id'),
     // счетчики для оптимизации
-    likes_count: integer().default(0),
-    repost_count: integer().default(0),
+    likes_count: integer()
+        .notNull()
+        .default(0),
+    repost_count: integer()
+        .notNull()
+        .default(0),
 
     ...created_at,
     ...updated_at
+}, (table) => {
+    return {
+        parentReference: foreignKey({
+            columns: [table.reply_post_id],
+            foreignColumns: [table.id],
+            name: "reply_post_id_fkey",
+        }).onDelete('set null'), // TODO: уточнить
+        // индексы для сортировок
+        likesCountIdx: index("likes_count_idx").on(table.likes_count),
+        CreatedAtIdx: index("created_at_idx").on(table.created_at),
+    };
 });
 
 export const postsRelations = relations(posts, ({one, many}) => ({
