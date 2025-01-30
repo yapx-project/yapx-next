c702edc8da2bbf91cfb148bfeff84ff0adff2caf
Dmitriy Pervin <theforner@gmail.com>
2025-01-30 20:53:02 +0500

    fix: change migrations dir

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,11 @@
-import {defineConfig} from "drizzle-kit";
+import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-    schema: './src/db/schema',
-    out: './migrations',
-    dialect: 'turso',
-    dbCredentials: {
-        url: process.env.TURSO_CONNECTION_URL!,
-        authToken: process.env.TURSO_AUTH_TOKEN!,
-    },
+  schema: "./src/db/schema",
+  out: "./src/db/migrations/",
+  dialect: "turso",
+  dbCredentials: {
+    url: process.env.TURSO_CONNECTION_URL!,
+    authToken: process.env.TURSO_AUTH_TOKEN!,
+  },
 });
--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  output: 'standalone',
+  output: "standalone",
 };
 
 export default nextConfig;
