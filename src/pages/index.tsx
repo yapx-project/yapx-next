8149bce5ea6801449564a1cf7c3c0602aac95ebd
Ro0kly <snallytango@yandex.ru>
2025-02-02 23:43:07 +0300

    auth_ui / configure fonts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,16 +4,18 @@ import { Inter, Roboto } from "next/font/google";
 const interFont = Inter({
   subsets: ['latin'],
   weight: ["400"],
+  variable: "--font-inter"
 });
 
 const robotoFont = Roboto({
   subsets: ['latin'],
-  weight: ["700"]
+  weight: ["700"],
+  variable: "--font-roboto"
 })
 
 export default function Home() {
   return (
-    <div className={`${interFont.className} ${robotoFont.className}`}>
+    <div className={`${interFont.variable} ${robotoFont.variable}`}>
       <AuthView />
     </div>
   )
