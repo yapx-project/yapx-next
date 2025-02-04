5243d357ee1f873529ace2c162c89e4bbf06fb09
Ro0kly <146754562+Ro0kly@users.noreply.github.com>
2025-02-04 17:28:27 +0300

    Auth UI (#17)
    * auth_ui / start creating auth UI
    
    * auth_ui / add fonts
    
    * auth_ui / configure fonts
    
    * auth_ui / add Input component
    
    * auth_ui / login page done
    
    * auth_ui / signup UI done
    
    * auth_ui / fix naming
    
    * auth_ui / fix lint
    
    * auth_ui / solve lint troubles

---
+++ src/pages/auth/sign-up-view.tsx
@@ -0,0 +1,82 @@
+import { useState } from "react";
+import Image from "next/image";
+import logo from "../styles/yapx_logo.png";
+import Input from "@/components/input";
+import ButtonBlue from "@/components/button_blue";
+import { InputEvent } from "@/types/events";
+
+export default function SignUpView() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordRep, setPasswordRep] = useState("");
+
+  const getName = (e: InputEvent) => {
+    setName(e.target.value);
+  };
+  const getEmail = (e: InputEvent) => {
+    setEmail(e.target.value);
+  };
+  const getLogin = (e: InputEvent) => {
+    setLogin(e.target.value);
+  };
+  const getPassword = (e: InputEvent) => {
+    setPassword(e.target.value);
+  };
+  const getPasswordRep = (e: InputEvent) => {
+    setPasswordRep(e.target.value);
+  };
+
+  return (
+    <div className="flex h-dvh flex-col justify-between bg-primary_gray">
+      <header className="flex h-14 items-center bg-white">
+        <Image className="ml-5" src={logo} alt="logo" />
+      </header>
+      <main className="m-5">
+        <h1 className="mb-4 font-roboto text-2xl">Регистрация</h1>
+        <form className="flex flex-col">
+          <div className="flex flex-col gap-4">
+            <Input
+              title="Имя"
+              placeholder="Ярополк"
+              type="text"
+              getValue={getName}
+              value={name}
+            />
+            <Input
+              title="Email"
+              placeholder="Yaropolk@yandex.ru"
+              type="email"
+              getValue={getEmail}
+              value={email}
+            />
+            <Input
+              title="Логин"
+              placeholder="Yaropolk"
+              type="text"
+              getValue={getLogin}
+              value={login}
+            />
+            <Input
+              title="Придумайте пароль"
+              placeholder="******"
+              type="password"
+              getValue={getPassword}
+              value={password}
+            />
+            <Input
+              title="Повторите пароль"
+              placeholder="******"
+              type="text"
+              getValue={getPasswordRep}
+              value={passwordRep}
+            />
+          </div>
+          <ButtonBlue className="mt-4" title="Зарегистрироваться" />
+        </form>
+      </main>
+      <footer className="h-14 border-t border-t-gray-300 bg-white"></footer>
+    </div>
+  );
+}
